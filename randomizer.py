1006ec3787aacbf8af985f2c5e8b88ed707c9df6
Anonymous <none>
2019-02-03 16:46:49 -0800

    initial commit, no randomization, some QoL patches

---
+++ randomizer.py
@@ -0,0 +1,39 @@
+from randomtools.tablereader import (
+    TableObject, get_global_label, tblpath, addresses, get_random_degree,
+    get_activated_patches, mutate_normal, shuffle_normal, write_patch)
+from randomtools.utils import (
+    classproperty, cached_property, get_snes_palette_transformer,
+    read_multi, write_multi, utilrandom as random)
+from randomtools.interface import (
+    get_outfile, get_seed, get_flags, get_activated_codes, activate_code,
+    run_interface, rewrite_snes_meta, clean_and_write, finish_interface)
+from collections import defaultdict
+from os import path
+from time import time, sleep, gmtime
+from collections import Counter
+from itertools import combinations
+
+
+VERSION = 1
+
+if __name__ == "__main__":
+    try:
+        print ("You are using the Super Adventure Island II "
+               "randomizer version %s." % VERSION)
+        print
+
+        ALL_OBJECTS = [g for g in globals().values()
+                       if isinstance(g, type) and issubclass(g, TableObject)
+                       and g not in [TableObject]]
+
+        codes = {}
+
+        run_interface(ALL_OBJECTS, snes=True, codes=codes, custom_degree=True)
+        clean_and_write(ALL_OBJECTS)
+        rewrite_snes_meta("SAI2-R", VERSION, lorom=False)
+
+        finish_interface()
+
+    except Exception, e:
+        print "ERROR: %s" % e
+        raw_input("Press Enter to close this program.")
